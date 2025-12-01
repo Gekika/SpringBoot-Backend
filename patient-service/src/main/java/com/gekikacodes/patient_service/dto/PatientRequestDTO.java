7a55fb029764feb38880f92f91d2fe0909ce0d1e
gekika <gkkaranja2@gmail.com>
2025-12-01 05:31:19 +0300

    Created a Request DTO for teh POst request

--- patient-service/src/main/java/com/gekikacodes/patient_service/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/gekikacodes/patient_service/dto/PatientRequestDTO.java
@@ -1,4 +1,66 @@
 package com.gekikacodes.patient_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class PatientRequestDTO {
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String fullname;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+    @NotBlank(message = "Date of Birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Registered date is required")
+    private String registeredDate;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+
 }
