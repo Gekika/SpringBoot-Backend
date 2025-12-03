dc9c182204f10177a5d0f7417044db0f68b0ddc9
gekika <gkkaranja2@gmail.com>
2025-12-03 08:19:06 +0300

    Added validation on the controllerto correctly display errors i

--- patient-service/src/main/java/com/gekikacodes/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/gekikacodes/patient_service/controller/PatientController.java
@@ -5,7 +5,9 @@ import com.gekikacodes.patient_service.dto.PatientRequestDTO;
 import com.gekikacodes.patient_service.dto.PatientResponseDTO;
 import com.gekikacodes.patient_service.service.PatientService;
 import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -38,7 +40,7 @@ public class PatientController {
         return ResponseEntity.ok().body(patientResponseDTO);
     }
     @PutMapping("/{id}")
-    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @RequestBody PatientRequestDTO patientRequestDTO){
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
 
         return ResponseEntity.ok().body(patientResponseDTO);
