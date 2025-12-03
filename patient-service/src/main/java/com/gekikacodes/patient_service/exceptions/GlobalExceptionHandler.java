0b4566dcbf565f0514891dc13fb434cae782d476
gekika <gkkaranja2@gmail.com>
2025-12-03 08:07:24 +0300

    Updated the Global exception handler for Patient not Found exception

--- patient-service/src/main/java/com/gekikacodes/patient_service/exceptions/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/gekikacodes/patient_service/exceptions/GlobalExceptionHandler.java
@@ -33,4 +33,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errors);
 
     }
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(
+            PatientNotFoundException ex){
+
+        log.warn("Patient not found {}", ex.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("mesage", "Patient not found");
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
